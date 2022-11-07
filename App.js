d60de20623c1a95a5149d378976a690cf28006a0
yadavian <ankit.rama.yadav@gmail.com>
2022-11-07 13:17:44 +0530

    device id added

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import messaging from '@react-native-firebase/messaging';
 import { Text } from 'react-native';
 import { firebase } from '@react-native-firebase/messaging/lib';
+import DeviceInfo from 'react-native-device-info';
+
+// or ES6+ destructured imports
 
 
 const App = () => {
 
-  const [token, setToken] = React.useState('')
+  const [token, setToken] = useState('')
+  const [deviceId, setDeviceId] = useState('')
+  console.log(`deviceId  =>`, deviceId)
 
   React.useEffect(() => {
 
@@ -17,9 +22,15 @@ const App = () => {
       const token = await defaultAppMessaging.getToken();
       console.log(`token  =>`, token)
       setToken(token)
+      console.log(`getDeviceId()  =>`, getDeviceId())
     }
 
     console.log(`getToken()  =>`, getToken())
+
+    DeviceInfo.getFirstInstallTime().then((firstInstallTime) => {
+      console.log(`firstInstallTime  =>`, firstInstallTime)
+      setDeviceId(firstInstallTime);
+    });
 
     async function requestUserPermission() {
       const authStatus = await messaging().requestPermission();
